9a2e0c103263dd400b1a4751fa40ca756a501390
melkybahia88@gmail.com <melkybahia88@gmail.com>
2023-10-21 18:01:40 -0300

    Add Operadores.java

--- tipo-variaveis/src/TiposVariaveis.java
+++ tipo-variaveis/src/TiposVariaveis.java
@@ -1,15 +1,21 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         // Tipos primitivos
         // Estudem a classe Strinf que reprenseta texto na aplicação
         
         String meuNome = "GLEYSON SAMPAIO";
 
+        System.out.print(meuNome);
+
         double salarioMinimo = 2500;
+
+        System.out.print(salarioMinimo);
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        System.out.print(numeroCurto2);
 
         int numero = 5;
 
@@ -18,5 +24,7 @@ public class TiposVariaveis {
         System.out.print(numero);
 
         final double VALOR_DE_PI = 3.14;
+
+        System.out.print(VALOR_DE_PI);
     }
 }
